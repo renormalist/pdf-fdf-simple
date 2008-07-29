a13d0323749eed9dd1c7b22bd3930e70d8224980
ss5 <ss5@e9a3fc86-3108-0410-97da-971987bdd377>
2008-07-29 20:40:43 +0000

    - Mini synopsis update about accessing single fields.
    git-svn-id: https://svn.renormalist.net/svn/pdf-fdf-simple/trunk@13 e9a3fc86-3108-0410-97da-971987bdd377

--- lib/PDF/FDF/Simple.pm
+++ lib/PDF/FDF/Simple.pm
@@ -19,7 +19,7 @@ PDF::FDF::Simple->mk_accessors(qw(
                                      attribute_id
                                 ));
 
-$VERSION = '0.17';
+$VERSION = '0.18';
 
 #Parse::RecDescent environment variables: enable for Debugging
 #$::RD_TRACE = 1;
