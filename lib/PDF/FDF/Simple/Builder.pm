437127cc060c640c6600e1902f78bb3d55202a31
Steffen Schwigon <ss5@renormalist.net>
2009-05-06 09:07:47 +0200

    Module::Build action "grammar" that updates precompiled parser
     ./Build grammar

--- lib/PDF/FDF/Simple/Builder.pm
+++ lib/PDF/FDF/Simple/Builder.pm
@@ -4,8 +4,25 @@ use strict;
 use warnings;
 
 use Module::Build;
+use File::Copy "mv";
+use Parse::RecDescent;
 
 use vars qw(@ISA);
 @ISA = ("Module::Build");
+
+sub ACTION_grammar
+{
+        require File::ShareDir;
+
+        my $grammar_file ='lib/auto/PDF/FDF/Simple/grammar';
+        open GRAMMAR_FILE, $grammar_file or die "Cannot open grammar file ".$grammar_file;
+        local $/;
+        my $grammar = <GRAMMAR_FILE>;
+        Parse::RecDescent->Precompile($grammar, "PDF::FDF::Simple::Grammar");
+        my $target = "lib/PDF/FDF/Simple/Grammar.pm";
+        mv "Grammar.pm", $target;
+        print "Updated $target\n";
+}
+
 
 1;
